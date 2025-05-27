02c4f94c3831f441cf891a3f0ed0a6593ab73452
nsrnannaisha <nisrinaannaisha@gmail.com>
2025-05-27 15:03:46 +0700

    fix api

--- src/main/java/id/ac/ui/cs/advprog/consultation/config/WebConfig.java
+++ src/main/java/id/ac/ui/cs/advprog/consultation/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("*")
-                .allowedOrigins("https://lucent-treacle-8937a6.netlify.app")
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
